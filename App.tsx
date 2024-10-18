6849ce238fb86c119664f99c525fdc014b73d8a4
cruasanya <98823717+cruasanya@users.noreply.github.com>
2024-10-18 18:40:30 +0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
+import { QrView } from './modules/qr-module';
+import { OnCodeScanned } from './modules/qr-module/src/QrModuleView';
 
 export default function App() {
+  const [scannerVisible, setScannerVisible] = useState(false);
+  const [qrArray, setQrArray] = useState<string[]>([])
+
+  const handleCodeScanned = (event: {nativeEvent: OnCodeScanned}) => {
+    setQrArray((prevArray) => [...prevArray, event.nativeEvent.data]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      {qrArray.length > 0 ? (
+          qrArray.map((code, index) => (
+            <Text key={index}>
+              {code}
+            </Text>
+          ))
+        ) : (
+          <Text>No QR codes scanned yet</Text>
+        )}
+      <Text>Scan your qr</Text>
+      <Pressable style={styles.button} onPress={ () => setScannerVisible(!scannerVisible)}>
+        <Text>Start</Text>
+      </Pressable>
+      <Modal animationType='slide' visible={scannerVisible}>
+        <View style={styles.container}>
+          <QrView style={{width: "75%", height: "75%" }} onCodeScanned={handleCodeScanned}/>
+          <Pressable style={styles.button} onPress={() => setScannerVisible(!scannerVisible)}>
+            <Text>Close</Text>
+          </Pressable>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -17,4 +45,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    borderRadius: 10,
+    padding: 15,
+    elevation: 2,
+    margin: 10,
+    backgroundColor: "yellow"
+  }
 });
